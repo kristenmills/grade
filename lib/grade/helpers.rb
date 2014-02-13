5e3f97f376c1e6496ec5b72e3b78c7dc66db8b98
kristenmills <kristen@kristen-mills.com>
2014-02-13 16:31:11 -0500

    activity journal stuff

--- lib/grade/helpers.rb
+++ lib/grade/helpers.rb
@@ -1,9 +1,11 @@
 require 'git'
 require 'colorize'
+require 'highline/import'
 
 module Grade
   module Helpers
     extend self
+
     # Pulls all of the repos
     def pull_and_checkout
       Dir.new('.').each do |directory|
@@ -17,6 +19,44 @@ module Grade
         puts "Reseting to commit #{commit.sha.cyan}"
         g.reset(commit)
       end
+    end
+
+    # Looks at everyones activity journals
+    def activity_journals
+      journals = YAML.load(File.open("#{CONFIG['project_name']}/journals.yml"))
+      journals = [] if journals.empty?
+
+      Dir.new('.').each do |directory|
+        next unless /[a-z]{2,3}\d{4}/.match(directory) and journals[directory].nil?
+        journals[directory] = activity_journal(directory)
+      end
+    ensure
+      File.open("#{CONFIG['project_name']}/journals.yml", 'w') do |f|
+        f.write(YAML.dump(journals))
+      end
+    end
+
+    # looks at an activity journal for a specific user
+    def activity_journal(user)
+      results = {}
+      puts "Displaying activity journal for #{user.blue}"
+      if File.exists("#{directory}/ActivityJournal.txt")
+        results[:has_journal] = 'yes'
+        journal = File.open("#{directory}/ActivityJournal.txt").
+        puts journal
+        choose do |menu|
+          menu.prompt('Is this acceptable?')
+          menu.choices(:y, :yes){ results[:acceptable] = 'yes'}
+          menu.choices(:n, :no){ results[:acceptable] = 'no'}
+        end
+        results[:notes] = ask('Additional notes: ')
+      else
+        puts "No activity journal for #{user}".red
+        results[:has_journal] = 'no'
+        results[:acceptable] = 'no'
+        results[:notes] = 'File not there'
+      end
+      results
     end
   end
 end
