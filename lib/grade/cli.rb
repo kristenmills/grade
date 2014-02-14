5c815ea90fbde47178e93f998e0b4e098bd490da
kristenmills <kristen@kristen-mills.com>
2014-02-14 12:14:17 -0500

    View code and combine

--- lib/grade/cli.rb
+++ lib/grade/cli.rb
@@ -14,9 +14,44 @@ module Grade
       if person.nil?
         Helpers.activity_journals
       else
-        Helpers.activity_journal(person)
+        person = person.dup
+        journals = YAML.load(File.open("_projects/#{CONFIG['project_name']}/journals.yml"))
+        journals = '' unless journals
+        journals = {} if journals.empty?
+        begin
+          journals[person] = Helpers.activity_journal(person)
+          journals = Hash[journals.sort]
+        ensure
+          File.open("_projects/#{CONFIG['project_name']}/journals.yml", 'w') do |f|
+            f.write(YAML.dump(journals))
+          end
+        end
       end
     end
-  end
 
+    desc 'view [dce]', 'views the code for everyone'
+    def view(person=nil)
+      if person.nil?
+        Helpers.view_code
+      else
+        person = person.dup
+        code = YAML.load(File.open("_projects/#{CONFIG['project_name']}/code.yml"))
+        code = '' unless code
+        code = {} if code.empty?
+        begin
+          code[person] = Helpers.view_code_for(person)
+          code = Hash[code.sort]
+        ensure
+          File.open("_projects/#{CONFIG['project_name']}/code.yml", 'w') do |f|
+            f.write(YAML.dump(code))
+          end
+        end
+      end
+    end
+
+    desc 'combine', 'combines all of the different files for a project'
+    def combine
+      Helpers.combine
+    end
+  end
 end
--- lib/grade/helpers.rb
+++ lib/grade/helpers.rb
@@ -22,7 +22,7 @@ module Grade
 
     # Looks at everyones activity journals
     def activity_journals
-      journals = YAML.load(File.open("Projects/#{CONFIG['project_name']}/journals.yml"))
+      journals = YAML.load(File.open("_projects/#{CONFIG['project_name']}/journals.yml"))
       journals = '' unless journals
       journals = {} if journals.empty?
 
@@ -31,7 +31,7 @@ module Grade
         journals[directory] = activity_journal(directory)
       end
     ensure
-      File.open("Projects/#{CONFIG['project_name']}/journals.yml", 'w') do |f|
+      File.open("_projects/#{CONFIG['project_name']}/journals.yml", 'w') do |f|
         f.write(YAML.dump(journals))
       end
     end
@@ -44,15 +44,75 @@ module Grade
         results['has_journal'] = 'yes'
         journal = File.open("#{user}/#{CONFIG['project_name']}/Activity_Journal.txt").read
         puts journal
-        results['acceptable'] = ask('Is this acceptable? ').to_s
+        results['points'] = ask('how many points is this worth?', Integer)
         results['notes'] = ask('Additional notes: ').to_s
       else
         puts "No activity journal for #{user}".red
         results['has_journal'] = 'no'
-        results['acceptable'] = 'no'
+        results['points'] = 0
         results['notes'] = 'File not there'
       end
       results
+    end
+
+    # View code for people
+    def view_code
+      code = YAML.load(File.open("_projects/#{CONFIG['project_name']}/code.yml"))
+      code = '' unless code
+      code = {} if code.empty?
+
+      Dir.new('.').each do |directory|
+        next unless /[a-z]{2,3}\d{4}/.match(directory) and code[directory].nil?
+        code[directory] = view_code_for(directory)
+      end
+    ensure
+      File.open("_projects/#{CONFIG['project_name']}/code.yml", 'w') do |f|
+        f.write(YAML.dump(code))
+      end
+    end
+
+    # View code for a specific user
+    def view_code_for(user)
+      results = {}
+      puts "Displaying code for #{user.cyan}"
+      CONFIG['project_files'].each do |file|
+        if File.exists?("#{user}/#{CONFIG['project_name']}/#{file}")
+          code = File.open("#{user}/#{CONFIG['project_name']}/#{file}").read
+          puts code
+        else
+          puts "No #{file} for #{user}".red
+        end
+      end
+      g = Git.open("#{user}")
+      g.log.each do {|x| puts x.message}
+      results['points'] = ask('How many points is this worth? ', Integer)
+      results['notes'] = ask('Aditional notes: ').to_s
+      results
+    end
+
+    def combine
+      hash = {}
+      Dir.new("_projects/#{CONFIG['project_name']}").each do |file|
+        next if file == '..' or file == '.' or file == 'combined.yml'
+        puts "Loading #{file.cyan}"
+        file_name = /(.*)\.yml/.match(file)[1]
+        yml = YAML.load(File.open("_projects/#{CONFIG['project_name']}/#{file}"))
+        yml.each do |user, values|
+          hash[user] ||= {}
+          hash[user][file_name] = values
+        end
+      end
+      hash.each do |user, projects|
+        points = 0
+        projects.each do |project,info|
+          points += info['points']
+        end
+        projects['total_points'] = points
+      end
+
+      File.open("_projects/#{CONFIG['project_name']}/combined.yml", 'w') do |f|
+        f.write(YAML.dump(hash))
+      end
     end
   end
 end
